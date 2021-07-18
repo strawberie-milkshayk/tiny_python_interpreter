2805a9bded3397bfe2fab6164c49f58fd4009c99
strawberie-milkshayk <87156585+strawberie-milkshayk@users.noreply.github.com>
2021-07-17 18:50:15 -0700

    updated files

--- python_interpreter.py
+++ python_interpreter.py
@@ -1,12 +1,9 @@
 import sys
-
 try:
     f = open(str(sys.argv[1]), 'r')
     data = f.read()
     f.close()
-    data.split('\n')
-    for line in data:
-        exec(line)
+    exec(data)
 except:
     try:
         exec(sys.argv[1])
